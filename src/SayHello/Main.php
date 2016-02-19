657d4ff054980bcf171218849a9a9b750e4dc43d
legoboy0215 <legoboy0215@users.noreply.github.com>
2016-02-19 13:22:49 +0800

    Improved lots of bugs.

--- src/SayHello/Main.php
+++ src/SayHello/Main.php
@@ -1,37 +1,36 @@
 <?php
+
   namespace SayHello;
+  
   use pocketmine\plugin\PluginBase;
-  use pocketmine\event\Listener;
   use pocketmine\utils\TextFormat as TF;
   use pocketmine\command\Command;
   use pocketmine\command\CommandSender;
-  use pocketmine\command\CommandExecutor;
   use pocketmine\Player;
-  class Main extends PluginBase implements Listener {
+  
+  class Main extends PluginBase{
+    
     public function onEnable() {
-      $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
+    
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
-      if($cmd->getName() === "warn") {
+      if($cmd->getName() === "hello") {
         if(!(isset($args[0])) {
           $sender->sendMessage(TF::RED . "Error: not enough args. Usage: /hello <player>");
           return true;
-        } else {
+        }else{
           $sender_name = $sender->getName();
           $player = $this->getServer()->getPlayer($args[0]);
           if($player === null) {
-            $sender->sendMessage(TF::RED . "Player " . $player . " could not be found.");
-          } else {
+            $sender->sendMessage(TF::RED . "Player " . $args[0] . " is not online.");
+          }else{
             $player_name = $player->getName();
-            unset($args[0]);
-            $reason = implode(" ", $args);
-            $player->sendMessage(TF::YELLOW . " . $sender_name . " says hello to you!");
-            $this->getServer()->broadcastMessage(TF::LIGHT_PURPLE . $sender_name . " said hello to " . $player_name . " "!");
-            $sender->sendMessage(TF::GREEN . "Success");
+            $player->sendMessage(TF::YELLOW . $sender_name . " says hello to you!");
+            $this->getServer()->broadcastMessage(TF::LIGHT_PURPLE . $sender_name . " said hello to " . $player_name . "!");
+            $sender->sendMessage(TF::GREEN . "Success!");
             return true;
           }
         }
       }
     }
   }
-?>
